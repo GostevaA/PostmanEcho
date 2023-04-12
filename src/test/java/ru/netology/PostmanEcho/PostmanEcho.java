8e2276c7b87420edf3f6067fc8c8daaf242f9752
Anna Gosteva <gostevaay@yandex.ru>
2023-04-12 13:29:58 +0300

    First commit

---
+++ src/test/java/ru/netology/PostmanEcho/PostmanEcho.java
@@ -0,0 +1,24 @@
+package ru.netology.PostmanEcho;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEcho {
+    @Test
+    public void shouldPostRawTextTest() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hello"))
+        ;
+    }
+}
